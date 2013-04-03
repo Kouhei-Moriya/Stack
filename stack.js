d0532acfbc57fbcd1092f458cdae0d3c11f5c9d0
Kouhei Moriya <kouhei@b1044147.(none)>
2013-04-03 17:06:01 +0900

    push

--- stack.js
+++ stack.js
@@ -5,15 +5,15 @@ function Stack(){
 function push(stk, data){
 	console.log(stk);
 	var temp = stk;
-	var stk = new Stack();
-	console.log(stk);
-	stk.value = data
+	console.log(temp);
+	stk.value = data;
 	stk.next = temp;
+	stk.next = new Stack();
 }
 function pop(){
 }
 
-var A = null;
+var A = new Stack();
 push(A, 1);
 push(A, 2);
 console.log(A);
