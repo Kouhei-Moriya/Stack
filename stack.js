e63cf0dd4c0174b666155297a612278d0ed74457
Kouhei Moriya <kouhei@b1044147.(none)>
2013-04-03 16:52:36 +0900

    second commit

--- stack.js
+++ stack.js
@@ -1,12 +1,21 @@
 function Stack(){
 	this.value = null,
-	this.next = null,
-	function push(){
-	}
+	this.next = null;
+}
+function push(stk, data){
+	console.log(stk);
+	var temp = stk;
+	var stk = new Stack();
+	console.log(stk);
+	stk.value = data
+	stk.next = temp;
+}
+function pop(){
 }
 
-
-var A = new Stack();
-console.log(A.value);
+var A = null;
+push(A, 1);
+push(A, 2);
+console.log(A);
 
 
