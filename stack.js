71e8514f707086cf1817f2f9ba6bf3848225dfe5
Kouhei Moriya <kouhei@b1044147.(none)>
2013-04-03 17:56:00 +0900

    push and pop

--- stack.js
+++ stack.js
@@ -1,21 +1,33 @@
-function Stack(){
+function Stacknode(){
 	this.value = null,
 	this.next = null;
 }
-function push(stk, data){
-	var temp = new Stack();
-	temp.value = data;
-	temp.next = stk;
-	stk = temp;
-	console.log(stk);
-	return stk;
+function Stack(){
+	this.first = new Stacknode();
 }
-function pop(){
+function push(stk, data){
+	var temp = new Stacknode();
+	temp.value = data;
+	temp.next = stk.first;
+	stk.first = temp;
+}
+function pop(stk){
+	var temp = stk.first.value;
+	stk.first = stk.first.next;
+	return temp;
 }
 
 var A = new Stack();
+console.log(A);
 push(A, 1);
-console.log(A);
-push(A, 2);
-console.log(A);
-
+console.log(A.first);
+var P = pop(A);
+console.log(P);
+push(A, 7);
+console.log(A.first);
+var P = pop(A);
+console.log(P);
+push(A, 57);
+console.log(A.first);
+var P = pop(A);
+console.log(P);
