099bfaa45be6adfbde4cb8c0bd0224ae65f0f24f
Kouhei Moriya <kouhei@b1044147.(none)>
2013-04-04 15:23:01 +0900

    qsort second commit

--- quicksort.js
+++ quicksort.js
@@ -1,33 +1,28 @@
 function quicksort(data){
+	if(data.length < 1) return;
 	sortfunc(0, data.length-1);
 	return data;	
 	function sortfunc(left,right){
-		if(left >= right) return;
-		var temp,
-		var i = left,
-		var j = right,
-		var p = (left + right)/2,
-		var pivot = data[p];
-		while(1){
-			while(i<=right){
-				if(data[i]>=pivot) break;
-				i++;
-			}
-			while(j>=left){
-				if(data[j]<=pivot) break;
-				j--;
-			}
-			if(i<j){
-				temp = data[i];
-				data[i] = data[j];
-				data[j] = temp;
-			}
-			i++;
-			j--;
+		if(right<=left) return;
+		var pivot = data[Math.floor((left + right) / 2)];
+		var l=left, r=right;
+		for(; 1; swap(l,r),l++,r--){
+			for(; l<=right; l++) if(data[l]>=pivot) break;
+			for(; left<=r; r--) if(data[r]<=pivot) break;
+			if(r<=l) break;
 		}
+		sortfunc(left, l-1);
+		if(l != left) sortfunc(l, right);
+		return;
+	}
+	function swap(i,j){
+		var temp = data[i];
+		data[i] = data[j],
+		data[j] = temp;
+		return;
 	}
 }
 
-
 var A = [9,6,10,4,2,7,11,3,1,5];
+A = quicksort(A);
 console.log(A);
