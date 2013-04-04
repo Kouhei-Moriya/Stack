22d8f3362f203c7ca7ba8b4bd30330d6dbe2e5a9
Kouhei Moriya <kouhei@b1044147.(none)>
2013-04-04 18:43:02 +0900

    hashmapcollision second commit

--- hash.js
+++ hash.js
@@ -1,37 +1,29 @@
 const N = 4;
-function hash(key){
-	var digit = 0;
-	for(i=0; i<key.length; i++) digit = digit * 256 + key.charCodeAt(i);
-	return digit % N;
-}
-/*function Mapl(N){
+function Hmap(){
 	this.key = new Array(N);
 	this.value = new Array(N);
-	this.addvalue = addvalue;
-	this.getvalue = getvalue;
-	function addvalue(k,v){
-		this.key[hash(k)] = k;
-		this.value[hash(k)] = v;
+	this.addvalue = function(k,v){
+		var p = hash(k);
+		for(i=0; i<N; i++,p=(p+1)%N) if(this.key[p] === void 0) break;
+		if(i==N) throw "配列が全て埋まっています";
+		this.key[p] = k;
+		this.value[p] = v;
 	}
-	function getvalue(k,v){
-		return this.value[hash(k)];
+	this.getvalue = function(k,v){
+		var p = hash(k);
+		for(i=0; i<N; i++,p=(p+1)%N) if(this.key[p]==k) break;
+		if(i==N) return undefined;
+		return this.value[p];
 	}
 	function hash(k){
+		var digit = 0;
+		for(i=0; i<k.length; i++) digit = digit * 256 + k.charCodeAt(i);
+		return digit % N;
+
 	}
-}*/
-
-
-var Map = new Array(N);
-Map.addvalue = function(key, value){
-	this[hash(key)] = value;
-}
-Map.getvalue = function(key){
-	return this[hash(key)];
 }
 
-
-
-
+var Map = new Hmap();
 
 Map.addvalue("apple", "りんご");
 Map.addvalue("orange", "みかん");
@@ -41,4 +33,4 @@ console.log(Map.getvalue("apple"));
 console.log(Map.getvalue("orange"));
 console.log(Map.getvalue("grape"));
 console.log(Map.getvalue("peach"));
-
+for(i=0; i<N; i++) console.log(i + " key:" + Map.key[i] + " value:" + Map.value[i]);
